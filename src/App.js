76ebecba4402937fef54ec12d462160ca44a1217
Lucasmes93 <lmessia93@gmail.com>
2023-03-28 16:29:22 +0200

    final Version

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (
     <Router>
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
+        <div class="container-fluid justify-content-center">
           <Link class="nav-link" to="/">Home</Link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="collapse navbar-collapse flex-grow-0" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <Link class="nav-link" to="/create">Create</Link>
